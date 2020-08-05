33550af14272526d4e0c9947e11c51dcd7fa01e0
Nicolas <nicolas123alejandro@gmail.com>
2020-08-05 18:21:58 -0500

    opciones completas para el caso de b = 0 y c = 0, ademas de optimizar el discriminante.

--- ecuaciones/segundo_grado.py
+++ ecuaciones/segundo_grado.py
@@ -7,11 +7,22 @@ b = float(input('Valor de b: '))
 c = float(input('Valor de c: '))
 
 if a != 0:
-    if b**2-4*a*c >= 0:
-        x1 = (-b + sqrt(b**2 - 4*a*c))/(2*a)
-        x2 = (-b - sqrt(b**2 - 4*a*c))/(2*a)
-        print('Soluciones: x1={0:.3f} y x2={1:.3f}'.format(x1,x2))
+    discriminante = b**2-4*a*c
+    if discriminante>= 0:
+        x1 = (-b + sqrt(discriminante))/(2*a)
+        x2 = (-b - sqrt(discriminante))/(2*a)
+        if x1 == x2:
+            print('Solución: x={0:.3f}'.format(x1))
+        else:
+            print('Soluciones: x1={0:.3f} y x2={1:.3f}'.format(x1,x2))
     else:
         print('No hay soluciones reales.')
 else:
-    print('No es una ecuación de segundo grado')
+    if b != 0:
+            x = -c/b
+            print('Solución: x={0:.3f}.'.format(x))
+    else:
+        if c!=0:
+            print('La ecuación no tiene solución.')
+        else:
+            print('La ecuación tiene infinitas soluciones.')
