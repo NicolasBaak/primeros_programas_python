2ad4f655ac6e59613a74ebc302783c96a2532760
Nicolas <nicolas123alejandro@gmail.com>
2020-08-05 18:33:14 -0500

    Se ingresan cinco números y se elige el mayor, otra opción con menos lineas de codigo

--- ecuaciones/maximo.py
+++ ecuaciones/maximo.py
@@ -1,8 +1,10 @@
-#Se ingresan tres números y se elige el mayor
+#Se ingresan cinco números y se elige el mayor
 
 a = int(input('Dame el primer número: '))
 b = int(input('Dame el segundo número: '))
 c = int(input('Dame el tercer número: '))
+d = int(input('Dame el cuarto número: '))
+e = int(input('Dame el quinto número: '))
 
 candidato = a
 
@@ -10,6 +12,11 @@ if b > candidato:
     candidato = b
 if c > candidato:
     candidato = c
+if d > candidato:
+    candidato = d
+if e > candidato:
+    candidato = e
+
 maximo = candidato
 
 print('El maximo es:', maximo)
