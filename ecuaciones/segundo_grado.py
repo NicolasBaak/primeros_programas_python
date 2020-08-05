72bbfbb54570750a1fc5cfa73f740f89fd2d6fb8
Nicolas <nicolas123alejandro@gmail.com>
2020-08-05 18:15:08 -0500

    opciones del discriminante positivo agregadas, en caso contrario mostrar que no existen soluciones reales.

--- ecuaciones/segundo_grado.py
+++ ecuaciones/segundo_grado.py
@@ -7,8 +7,11 @@ b = float(input('Valor de b: '))
 c = float(input('Valor de c: '))
 
 if a != 0:
-    x1 = (-b + sqrt(b**2 - 4*a*c))/(2*a)
-    x2 = (-b - sqrt(b**2 - 4*a*c))/(2*a)
-    print('Soluciones: x1={0:.3f} y x2={1:.3f}'.format(x1,x2))
+    if b**2-4*a*c >= 0:
+        x1 = (-b + sqrt(b**2 - 4*a*c))/(2*a)
+        x2 = (-b - sqrt(b**2 - 4*a*c))/(2*a)
+        print('Soluciones: x1={0:.3f} y x2={1:.3f}'.format(x1,x2))
+    else:
+        print('No hay soluciones reales.')
 else:
     print('No es una ecuación de segundo grado')
